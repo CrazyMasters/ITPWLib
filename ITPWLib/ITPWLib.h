7c2e6cbc9beb20e565d11abca02377015bf44759
Vladislav Permyakov <vladic.p@gmail.com>
2022-03-21 10:23:54 +0300

    Initial Commit

---
+++ ITPWLib/ITPWLib.h
@@ -0,0 +1,18 @@
+//
+//  ITPWLib.h
+//  ITPWLib
+//
+//  Created by Permyakov Vladislav on 21.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ITPWLib.
+FOUNDATION_EXPORT double ITPWLibVersionNumber;
+
+//! Project version string for ITPWLib.
+FOUNDATION_EXPORT const unsigned char ITPWLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITPWLib/PublicHeader.h>
+
+
